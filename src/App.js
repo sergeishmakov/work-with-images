f3f1e26b6c25865cce3be5748001d5a686cc79f0
sergeishmakov <m.shmakov.sergei@yandex.ru>
2021-10-29 16:36:47 +0300

    Added filters

--- src/App.js
+++ src/App.js
@@ -1,15 +1,36 @@
 import { useCallback, useEffect, useState } from "react";
 import Switch from "react-switch";
+import { capitalize, startCase } from "lodash";
 
 import { getBrightnessChartData, getImageData, getRGBChartData } from "./utils";
 
 import "./App.css";
 import { drawBrightnessChart, drawRGBChart } from "./tools";
+import { grayscale, invert } from "./convertMethods";
+
+const Filters = {
+  NONE: "none",
+  INVERSION: "inverse",
+  BLACK_WHITE: "black_white",
+};
+
+const imageFilters = {
+  [Filters.NONE]: "none",
+  [Filters.INVERSION]: "invert(100%)",
+  [Filters.BLACK_WHITE]: "grayscale(100%)",
+};
+
+const filterMethods = {
+  [Filters.NONE]: null,
+  [Filters.INVERSION]: invert,
+  [Filters.BLACK_WHITE]: grayscale,
+};
 
 function App() {
   const [imageUrl, setImageUrl] = useState(null);
-  const [imageData, setImageData] = useState(null);
   const [isColor, setIsColor] = useState(false);
+  const [filter, setFilter] = useState(Filters.NONE);
+  const [img, setImg] = useState(null);
 
   const [canvas, setCanvas] = useState(null);
 
@@ -24,7 +45,7 @@ function App() {
     img.src = url;
     img.style.filter = "invert(100%)";
     img.onload = function () {
-      setImageData(getImageData(this));
+      setImg(this);
     };
   };
 
@@ -32,20 +53,25 @@ function App() {
     if (node) setCanvas(node);
   }, []);
 
-  const createBarChart = useCallback(() => {
-    if (isColor) return drawRGBChart(canvas, getRGBChartData(imageData));
-    return drawBrightnessChart(canvas, getBrightnessChartData(imageData));
-  }, [canvas, imageData, isColor]);
+  const createBarChart = useCallback(
+    (data) => {
+      if (isColor) return drawRGBChart(canvas, getRGBChartData(data));
+      return drawBrightnessChart(canvas, getBrightnessChartData(data));
+    },
+    [canvas, isColor]
+  );
+
+  useEffect(() => {
+    if (img && canvas) {
+      const newImageData = getImageData(img, filterMethods[filter]);
+      if (!newImageData) return;
+      createBarChart(newImageData);
+    }
+  }, [canvas, createBarChart, filter, img]);
 
   const handleIsColorChange = (value) => {
     setIsColor(value);
   };
-
-  useEffect(() => {
-    if (canvas && imageData) {
-      createBarChart();
-    }
-  }, [canvas, createBarChart, imageData]);
 
   return (
     <div className="App">
@@ -57,8 +83,20 @@ function App() {
         <input type="file" accept="image/*" onChange={handleFileUpload} />
       </div>
       <div className="row">
+        {Object.values(Filters).map((item) => (
+          <label className="checkbox">
+            <input
+              type="checkbox"
+              checked={filter === item}
+              onChange={() => setFilter(item)}
+            />
+            {capitalize(startCase(item))}
+          </label>
+        ))}
+      </div>
+      <div className="row">
         <img
-          style={{ filter: "invert(100%)" }}
+          style={{ filter: imageFilters[filter] }}
           src={imageUrl}
           width="300px"
           alt=""
